17b9286fb9d02573c30084fbc387af295b002313
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-05-11 13:55:03 -0400

    create new dirs for each jg run

--- RUNME.sh
+++ RUNME.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ $# -ne 1 ]];then
+	exit 1
+fi
+
+
+output_dir="$(date +"%Y%m%dT%H%M%S")--jgproject"
 export CVA6_REPO_DIR=$HOME/cva6_fpga
-jg -tcl cva6.tcl
+time jg -no_gui -tcl $1 -proj "jasper_outputs/$output_dir"
